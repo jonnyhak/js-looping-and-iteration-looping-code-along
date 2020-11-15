d65b62f9c1d7c40ec4999a619fe5050bb93fcff3
jonnyhak <jonnyhak@gmail.com>
2020-11-15 18:29:34 -0500

    commit tree 916a086f9dd5c0eb74b00fa6cc5a591352ab7ddd

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+
+
+function writeCards(names) {    
+    let thanks = []
+    for (let i = 0; i < names.length; i++) {
+        const name = names[i];
+        thanks.push(`Thank you, ${name}, for the wonderful surprise gift!`)
+    }
+    return thanks 
+}
+
+function countDown(num){
+
+    let i = num
+    while (i >= 0) {
+        console.log(i)
+        i--
+    }
+}
+ 
